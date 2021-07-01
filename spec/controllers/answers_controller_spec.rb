778e9def657d5779255eee0aed46815bffa9abb9
ME <forzamaks@gmail.com>
2021-07-01 17:23:10 +0300

    Add history Create question, Create answer to question
    QuestionsControler: #index, #show, #new, #create
    AnswersController: #new, #create

---
+++ spec/controllers/answers_controller_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe AnswersController, type: :controller do
+  let(:answer) { create(:answer) }
+  let(:question) { create(:question) }
+
+  describe 'GET #new' do
+    before { get :new, params: { question_id: question } }
+
+    it 'renders new view' do
+      expect(response).to render_template :new 
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'save a new answer in the database' do
+        count = Answer.count
+
+        expect { post :create, params: { question_id: question, answer: attributes_for(:answer) } }.to change(Answer, :count).by(1)
+      end
+      it 'redirect to show' do
+        post :create, params: { question_id: question, answer: attributes_for(:answer) }
+        expect(response).to redirect_to assigns(:question)
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'does not save the answer' do
+        expect { post :create, params: { question_id: question, answer: attributes_for(:answer, :invalid) } }.to_not change(Answer, :count)
+      end
+
+      it 're-render new' do
+        post :create, params: { question_id: question, answer: attributes_for(:answer, :invalid) }
+        expect(response).to render_template :new
+      end
+    end
+  end
+end
---
+++ spec/controllers/questions_controller_spec.rb
@@ -0,0 +1,58 @@
+require 'rails_helper'
+
+RSpec.describe QuestionsController, type: :controller do
+  let(:question) { create(:question) }
+
+  describe 'GET #index' do
+    let(:questions) { create_list(:question, 3) }
+    before { get :index }
+    it 'populates an array of all questions' do
+      expect(assigns(:questions)).to match_array(questions)
+    end
+    it 'renders index view' do
+      expect(response).to render_template :index 
+    end
+  end
+
+  describe 'GET #show' do
+    
+    before { get :show, params: { id: question } }
+
+    it 'renders show view' do
+      expect(response).to render_template :show 
+    end
+  end
+
+  describe 'GET #new' do
+    before { get :new }
+
+    it 'renders new view' do
+      expect(response).to render_template :new 
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'save a new question in the database' do
+        count = Question.count
+
+        expect { post :create, params: { question: attributes_for(:question) } }.to change(Question, :count).by(1)
+      end
+      it 'redirect to show' do
+        post :create, params: { question: attributes_for(:question) }
+        expect(response).to redirect_to assigns(:question)
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'does not save the question' do
+        expect { post :create, params: { question: attributes_for(:question, :invalid) } }.to_not change(Question, :count)
+      end
+      it 're-render new' do
+        post :create, params: { question: attributes_for(:question, :invalid) }
+        expect(response).to render_template :new
+      end
+    end
+  end
+
+end
