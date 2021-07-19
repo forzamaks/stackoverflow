25704b0bdf54c95ee9af0fc538d026f9a2bff25d
ME <forzamaks@gmail.com>
2021-07-19 17:23:15 +0300

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,6 +2,7 @@
 # ======================
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
+require "capistrano/sidekiq"
 
 server "206.189.109.84", user: "deploy", roles: %w{app db web}, primary: true
 set :rail_env, :production
