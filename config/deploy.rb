85fcf4005ec9e3cd639a4ee5ffcba3b6678a2a54
ME <forzamaks@gmail.com>
2021-07-19 12:08:34 +0300

    add cpistrano

---
+++ config/deploy.rb
@@ -0,0 +1,17 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.16.0"
+
+set :application, "qna"
+set :repo_url, "git@github.com:forzamaks/stackoverflow.git"
+
+
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, "/home/deploy/qna"
+set :deploy_user, 'deploy'
+
+# Default value for :linked_files is []
+append :linked_files, "config/database.yml", 'config/master.key'
+
+# Default value for linked_dirs is []
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", 'storage'
---
+++ config/deploy/production.rb
@@ -0,0 +1,26 @@
+# server-based syntax
+# ======================
+# Defines a single server with a list of roles and multiple properties.
+# You can define all roles on a single server, or split them:
+
+server "206.189.109.84", user: "deploy", roles: %w{app db web}, primary: true
+set :rails_env, :production
+
+
+
+# Custom SSH Options
+# ==================
+# You may pass any option but keep in mind that net/ssh understands a
+# limited set of options, consult the Net::SSH documentation.
+# http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start
+#
+# Global options
+# --------------
+ set :ssh_options, {
+   keys: %w(/Users/me/.ssh/id_rsa),
+   forward_agent: true,
+   auth_methods: %w(publickey password),
+   port: 2222
+ }
+
+
