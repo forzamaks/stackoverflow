992208041efbc3ae0d3c13ac88fb187dc03ab922
ME <forzamaks@gmail.com>
2021-07-19 18:08:05 +0300

    fix actionCable

--- app/javascript/channels/answers_channel.js
+++ app/javascript/channels/answers_channel.js
@@ -1,18 +1,19 @@
 import consumer from "./consumer"
-
-consumer.subscriptions.create({ channel: "AnswersChannel", question_id: gon.question_id }, {
-  connected() {
-    return this.perform("follow")
-  },
-  received(data) {
-    if (gon.current_user.id == data.user_id) {
-
-      let answerTemplate = require('channels/templates/answer.hbs')({
-          params: data,
-          user_id: gon.current_user.id,
-          is_question_author: gon.current_user.id === data.question_author_id
-      })
-      $('.answers').append(answerTemplate)
-    }
-  },
+$(document).on('turbolinks:load',function(){
+  consumer.subscriptions.create({ channel: "AnswersChannel", question_id: gon.question_id }, {
+    connected() {
+      return this.perform("follow")
+    },
+    received(data) {
+      if (gon.current_user.id == data.user_id) {
+  
+        let answerTemplate = require('channels/templates/answer.hbs')({
+            params: data,
+            user_id: gon.current_user.id,
+            is_question_author: gon.current_user.id === data.question_author_id
+        })
+        $('.answers').append(answerTemplate)
+      }
+    },
+  })
 })
--- app/javascript/channels/comments_channel.js
+++ app/javascript/channels/comments_channel.js
@@ -1,19 +1,19 @@
 import consumer from "./consumer"
-
-consumer.subscriptions.create({ channel: "CommentsChannel", question_id: gon.question_id }, {
-  connected() {
-    return this.perform("follow")
-  },
-  received(data) {
-    var type = data.comment.commentable_type.toLowerCase();
-    var id = data.comment.commentable_id;
-
-    if (type == 'question') {
-      $('.question-comment').append(data['partial']);
-    } else {
-      $(`.answer-comment[data-comment-answer=${id}]`).append(data['partial']);
+$(document).on('turbolinks:load',function(){
+  consumer.subscriptions.create({ channel: "CommentsChannel", question_id: gon.question_id }, {
+    connected() {
+      return this.perform("follow")
+    },
+    received(data) {
+      var type = data.comment.commentable_type.toLowerCase();
+      var id = data.comment.commentable_id;
+      if (type == 'question') {
+        $('.question-comment').append(data['partial']);
+      } else {
+        $(`.answer-comment[data-comment-answer=${id}]`).append(data['partial']);
+      }
+      $('.new-comment #comment_body').val('');
     }
-    $('.new-comment #comment_body').val('');
-  }
 
+  })
 })
