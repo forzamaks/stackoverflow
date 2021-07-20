3be41c5638ad0ab7a12346d3930cb6f7d84b577f
ME <forzamaks@gmail.com>
2021-07-20 17:41:24 +0300

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,9 +8,7 @@ server "206.189.109.84", user: "deploy", roles: %w{app db web}, primary: true
 set :rail_env, :production
 set :branch, 'main'
 
-set :rvm_map_bins, [ 'bundle', 'rails', 'rake', 'setup', 'webpack', 'webpack-dev-server', 'yarn' ]
-set :pty,  false
-set :sidekiq_monit_use_sudo, false
+set :rvm_map_bins, [ 'bundle', 'rails', 'rake', 'setup', 'webpack', 'webpack-dev-server', 'yarn', 'sidekiq' ]
 
 # Custom SSH Options
 # ==================
