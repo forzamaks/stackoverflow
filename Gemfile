f5a902f0886aaa7da7fa9c91fa63c4a2391f8224
ME <forzamaks@gmail.com>
2021-07-21 13:01:51 +0300

    Add caching to question page

--- Gemfile
+++ Gemfile
@@ -50,6 +50,7 @@ gem 'whenever', require: false
 gem 'mysql2'
 gem 'thinking-sphinx'
 gem 'unicorn'
+gem 'redis-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,11 @@ module ApplicationHelper
   def flash_class_name(name)
     TYPES_FLASH[name.to_sym] || name
   end
+
+  def collection_cache_key_for(model)
+    klass = model.to_s.capitalize.constantize
+    count = klass.count
+    max_updated_at = klass.maximun(:updated_at).try(:utc).try(:to_s, :number)
+    "#{model.to_s.pluralize}/collection-#{count}-#{max_updated_at}"
+  end
 end
